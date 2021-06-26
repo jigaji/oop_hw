81713c777f0adc663ab2a7281fcdca228ac57274
Saglara Tserenova <ts.saglara@gmail.com>
2021-06-26 19:55:04 +0300

    Task2

--- HW.py
+++ HW.py
@@ -13,11 +13,19 @@ class Student:
         self.courses_in_progress = []
         self.grades = {}
 
+    def l_grade(self, lecture, course, grade):
+        if isinstance(lecture, Lecture) and course in lecture.courses_attached and course in self.courses_in_progress:
+            if course in lecture.grades:
+                lecture.grades[course] += [grade]
+            else:
+                lecture.grades[course] = [grade]
+        else:
+            return 'Ошибка'
 
 class Lecture(Mentor):
     def __init__(self, name, surname):
-        super.__init__(name, surname)
-
+        super().__init__(name, surname)
+        self.grades = {}
 
 
 class Reviewer(Mentor):
@@ -25,3 +33,35 @@ class Reviewer(Mentor):
         super().__init__(name, surname)
 
 
+    def rate_hw(self, student, course, grade):
+        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
+            if course in student.grades:
+                student.grades[course] += [grade]
+            else:
+                student.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+
+best_student = Student('Ruoy', 'Eman', 'your_gender')
+best_student.courses_in_progress += ['Python']
+
+
+cool_reviewer = Reviewer('Re', 'Viewer')
+cool_reviewer.courses_attached += ['Python']
+
+
+cool_reviewer.rate_hw(best_student, 'Python', 10)
+cool_reviewer.rate_hw(best_student, 'Python', 10)
+cool_reviewer.rate_hw(best_student, 'Python', 10)
+cool_reviewer.rate_hw(best_student, 'Python', 10)
+
+print(best_student.grades)
+
+soso_lecture = Lecture('Lec', 'Ture')
+soso_lecture.courses_attached += ['Python']
+good_student = Student('Lee', 'Bruce', 'male')
+good_student.courses_in_progress += ['Python']
+best_student.l_grade(soso_lecture, 'Python', 7)
+good_student.l_grade(soso_lecture, 'Python', 6)
+
+print(soso_lecture.grades)
